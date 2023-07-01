0bb53f142dda0c1ab2fc7d853c88c1a76cc07270
Iordanis Fostiropoulos <fostiropoulos@users.noreply.github.com>
2023-07-01 16:03:43 +0000

    include trial_uid on the results

--- ablator/analysis/results.py
+++ ablator/analysis/results.py
@@ -70,7 +70,10 @@ def read_result(config_type: type[ConfigBase], json_path: Path) -> pd.DataFrame:
         )
         df = pd.read_json(json_path)
         df = pd.concat([pd.DataFrame([experiment_attributes] * len(df)), df], axis=1)
-        return df.reset_index()
+        df.index.name = "step"
+        df.reset_index(inplace=True)
+        df["trial_uid"] = json_path.parent.name
+        return df.set_index(["trial_uid",'step'])
 
     except builtins.Exception:
         traceback.print_exc()
@@ -226,7 +229,7 @@ class Results:
     def read_results(
         cls,
         config_type: type[ConfigBase],
-        experiment_dir: Path,
+        experiment_dir: Path | str,
         num_cpus=None,
     ) -> pd.DataFrame:
         """
@@ -238,7 +241,7 @@ class Results:
         ----------
         config_type : type[ConfigBase]
             The configuration class
-        experiment_dir : Path
+        experiment_dir : Path | str
             The experiment directory
         num_cpus : int, optional
             Number of CPUs to use for ray processing, by default ``None``
@@ -249,7 +252,7 @@ class Results:
             A dataframe of all the results
         """
         results = []
-        json_paths = list(experiment_dir.rglob("results.json"))
+        json_paths = list(Path(experiment_dir).rglob("results.json"))
         if len(json_paths) == 0:
             raise RuntimeError(f"No results found in {experiment_dir}")
         cpu_count = mp.cpu_count()
